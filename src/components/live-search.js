0a4ca6337a7ee8b5b7bf2a7e3312c25202161dcb
Joe Turner <joe@oampo.co.uk>
2017-03-31 12:33:48 +0100

    Add solution

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -3,11 +3,25 @@ import React from 'react';
 import SearchInput from './search-input';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
-    return (
-        <div className="live-search">
-            <SearchInput />
-            <CharacterList characters={props.characters} />
-        </div>
-    );
+export default class LiveSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+    }
+
+    render() {
+        const characters = this.props.characters.filter(character =>
+            character.name.toLowerCase().includes(
+                this.state.searchTerm.toLowerCase()
+            )
+        );
+        return (
+            <div className="live-search">
+                <SearchInput onChange={searchTerm => this.setState({searchTerm})} />
+                <CharacterList characters={characters} />
+            </div>
+        );
+    }
 }
--- src/components/search-input.js
+++ src/components/search-input.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function SearchInput(props) {
-    return <input type="search" name="Search" placeholder="Dale Cooper" />;
+    return (
+        <input type="search" name="Search" placeholder="Dale Cooper"
+               onChange={e => props.onChange(e.target.value)} />
+    );
 }
