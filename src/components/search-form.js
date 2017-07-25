10e97e02a47184b13136c53e1c45626536762b64
Elias Mason <eliasjmason@gmail.com>
2017-07-25 16:22:11 -0700

    Add aria-live span for alerting results

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 export default function SearchForm(props) {
+  const countNoun = props.characterCount === 1 ? 'result' : 'results'
     return (
         <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="search">Search</label>&emsp;
             <input type="search" id="search" name="search" placeholder="Dale Cooper" 
                 onChange={e => props.onChange(e.target.value)} />
+            <span className="results-count" 
+              aria-live="assertive" 
+              aria-atomic="true">
+                {props.characterCount} {countNoun}
+              </span>
         </form>
     );
 }
