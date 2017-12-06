ee6b97524d2d9111f7b126fd0e3e196c40209a80
Elias Mason <eliasjmason@gmail.com>
2017-12-06 04:43:12 -0800

    Minor cleanup

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import SearchForm from './search-form';
-import CharacterList from './character-list';
 import CharacterCount from './character-count';
+import CharacterList from './character-list';
 
 
 export default class LiveSearch extends React.Component {
